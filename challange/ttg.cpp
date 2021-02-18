ca6cf126336c9ddd730218a5c09f627d17d670f1
An-1999 <79268348+An-1999@users.noreply.github.com>
2021-02-18 19:32:50 +0400

    Add files via upload

---
+++ challange/ttg.cpp
@@ -0,0 +1,17 @@
+tic_tac_toe
+
+#include <iostream>
+
+
+int main()
+{
+    std::cout << " xax@ sksum eq? xndrum em mutqagreq Y or N" << std::endl;
+    char first;
+    std::cin >> fisrt;
+    if (first == 'Y') {
+    } else if (first == 'N') {
+    } else {
+        std::cout << "katarvel e sxal mutq" << std::endl;
+        return 0;
+    }
+}
